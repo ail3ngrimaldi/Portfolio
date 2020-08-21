3eee222a2c7717a56983d7bf3ef4e6b8da38cac3
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2020-08-21 20:17:52 -0300

    ThemeProvider added to index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import ButtonAppBar from './components/ButtonAppBar';
+
 
 function App() {
   return (
     <div className="App">
+      <ButtonAppBar />
       <header className="App-header">
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ButtonAppBar from './components/ButtonAppBar';
+import App from './App';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { purple, green } from '@material-ui/core/colors';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ButtonAppBar />
-
+    <ThemeProvider theme={theme} >
+      <App/>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

a90719629dcf4685599540e3a9b7b4e3893fbb80
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2020-09-09 22:31:07 -0300

    Favicon changed

---
+++ build/precache-manifest.d35539de6e5fc09b1e3b2618bc187c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71a9ced6a8dc3a584c7b2726ae93eef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e56a49f84d1c346cb65",
+    "url": "/static/css/main.4545595e.chunk.css"
+  },
+  {
+    "revision": "22956feb18f76408afb5",
+    "url": "/static/js/2.f5dfc494.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.f5dfc494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e56a49f84d1c346cb65",
+    "url": "/static/js/main.0b0a8055.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "3b86b747b766d32cdc6bbc5aeac03c59",
+    "url": "/static/media/avatar.3b86b747.jpeg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e89013fa4525858d8e1203d67bc3252.js"
+  "/precache-manifest.d35539de6e5fc09b1e3b2618bc187c5c.js"
 );
 
 self.addEventListener('message', (event) => {
